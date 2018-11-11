fdf0667a4049b4fa64b4a03d3b00ea17e37730bc
Samuel Snopko <samuell.snopko@gmail.com>
2018-11-11 17:38:52 +0100

    added second base text content

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,11 @@
       <p>Sind so etwas wie eine leichte und schlanke Schrift wirkt elegant, oder eine fette und schwere wirkt stark. Weiter wirkt eine geneigte Schrift dynamisch, und einee handschriftliche zum Beispiel menschlich und verbunden. Die Usability News Studie 2006 hat da einige Untersuchungen zu gemacht.</p>
     </BaseText>
     <AnimationHead/>
+    <BaseText class="under-head-text">
+      <h4>Direkte Assoziationen</h4>
+      <p>Assoziationen beziehen sich auf zurückligende Verbindungen mit dieser Schrift, gesellschaftlich und aber auch persönlich: “So wird beispielsweise die Helvetica-Schriftart auf den Formularen des United States Tax and Internal Revenue Service verwendet, was die Wahrnehmung der Schriftart beeinflusst - abhängig von der Erfahrungen mit dem Steuersystem.”</p>
+      <p>Obit, temporis eum voluptas ipsandi odignis tiasitatur Iqui dolut fuga. Nam volorib ustiasperro qui veligni intis nimodit pre iurios nonsequam re ressequi officiaerat. Tatem eati omnihil iquibea ne veniste si res acepro tem evelestrum sam, core, nonse cullenditios enietur re nusciundant am, none occulpa ipissimus eos simetusda pre, as molorerore conse perspedi dolor maio officipsanda et esti aborendis aperepratur, solecaborum.</p>
+    </BaseText>
     <div style="height: 20rem;"/>
   </div>
 </template>
@@ -33,23 +38,21 @@ export default {
 <style lang="scss" scoped>
 @import "./assets/scss/toolbelt";
 
-.logo {
-  display: block;
-  width: 100vh;
-  height: auto;
-  max-width: rem(300);
-  margin-top: 20vh;
-  margin-left: auto;
-  margin-right: auto;
-}
+.under-head-text {
+  position: relative;
+  z-index: 1;
+  margin-top: rh(-4);
 
-h1,
-h2,
-.links {
-  text-align: center;
-}
+  @include breakpoint(medium) {
+    margin-top: rh(-8);
+  }
 
-.links {
-  margin: rem(20);
+  @include breakpoint(xlarge) {
+    margin-top: rh(-10);
+  }
+
+  @include breakpoint(xxlarge) {
+    margin-top: rh(-8);
+  }
 }
 </style>

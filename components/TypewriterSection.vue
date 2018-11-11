c08fa05e6f2d8997bc5498142532728992371f8c
Samuel Snopko <samuell.snopko@gmail.com>
2018-11-11 21:54:42 +0100

    added comp book animation to the typewriter section

--- components/TypewriterSection.vue
+++ components/TypewriterSection.vue
@@ -19,28 +19,23 @@
           <p class="quote">„Hier steht ein schönes Zitat zum Thema, lorem esci mus Iqui blab ipita.“</p>
           <p>Lo bernatusdam re min cupiste dolupta temquatur minus, nimusciet volut faccust ania comnihillab int volupicatem excepe velecta andit et arum que pellaut et occuptur. Duciliquae volorro voluptia pe volent ut aperibus quae volores citatur atati aut utem voles aid miniend andunt.</p>
         </div>
-        <!--
-        <h4>Zusammengenommen wirkt es so</h4>
-        <p>Beide assoziativen Ebenen wirken zusammen auf die emotionale Wahrnehmung von Schriften. Erstens - Wo wurde die Schrift schon mal gesehen? Zweitens - Welche Inhalte und Themen verbindet man mit der Schrift? Als Drittes - Was für persönliche Verbindungen hat man mit den Themen - gut oder schlecht? Und als viertes und letztes - Stimmt der Kontext in dem sie steht? Nequas et ma delescimus di que nistia cus, quam quae sita verum harcim harcide liquam, temporeri sam qui cuptatent occusan ditioruptat voloreiciis rem que dis quoditint.</p>
-        <p>Acientore, cusapedit, saperum quiderrovit as sinvenim velit ma que minvel et aut officienim enda quiamen dempossum eic te cus enihillo evelend unduntis recerum rehenda ma delescimus di que nistia.</p>
-        <div
-          v-view="handleStopSignView"
-          :class="stopSignClasses"
-          class="stop-sign--wrapper">
-          <StopSign class="stop-sign"/>
+        <div class="lottie--comp-book-wrapper">
+          <div
+            v-view="handleCompBookView"
+            :class="compBookClasses"
+            class="lottie--comp-book">
+            <Lottie
+              :options="compBookOptions"
+              @animCreated="handleCompBook"/>
+          </div>
         </div>
-        <div class="placeholder--stop-sign"/>
-        <h2>So wird die Wirkung von Schriften genutzt</h2>
-        <p>Lorem ipsum dolor ctibea voloriatum quiasit eossent mi, que consed quis dunt, simetur sandigent occum renistotae nosae repeliatem quuntorum eos as iunt cim qui officiunt quia sitate nonsectur sequo dolupiendam dolorita dolupta spelendebis esseque pellaturem re, ellabore laboresequi di omnim et mod quas di re re nonseque odisitem volorae net omnim elliquiae volupitiusam et.</p>
-        <h3 class="is-h4">Funktionalität von Schrift</h3>
-        <p>„Aufgaben scheinen einfacher zu sein, wenn sie leichter zu lesen sind“ anders rum kann man es auch ausnutzen „Wenn eine Speisekarte schwieriger zu lesen ist, entsteht der Eindruck, dass die Speisen schwieriger herzustellen sind.“ Em eum et porrum remquoditin cus. Tem eturia coriberume etur antiam ra nos endiam, consequam into toreceat dolupic iendiate dolupiet ra dolupta tusdandaeped ullant as quo consequi amusapis mintis ex ea sunt omnim et mod quas di re re nonseque pa volor sedi omnia dolupta temporem. Nequam harum volut quasi omnis sendisit que quid qui untis dem accum dolorpor reped.</p>
-        <p>Quo tem quas magnatae ad ut omnihil iquaerum consecaborem quam autendit, que non et occus, sit auta eatis nobistr untur? Qui acia sequis ut et entecto ex essimil esed mint audam simus vellaut atibus dolupta temporro tem conse pellorro molo te ommolendis debitis ulloreius eum eos et adic tecepta quideriaerum reserch illabora cupis dolectur.</p>
-        <h3 class="is-h4">Selbst mit unserer Zunge lässt sich Schrift spüren</h3>
-        <p>Sarah Hyndman hat in Studien festgestellt das gleiche Speisen oder Getränke anders schmecken, je nachdem wie die Schrift darauf war. Lorem ipsum dolor Nequas et ma delescimus di que nistia cus, quam quae sita verum harcim harcide liquam, temporeri sam qui cuptatent occusan ditioruptat voloreiciis rem que dis quoditint, acientore, cusapedit, saperum quiderrovit as sinvenim velit ma que minvel et aut officienim enda quiamen dempossum eic te cus enihillo evelend unduntis recerum.</p>
-        <div class="quote-wrapper">
-          <p class="quote">„Hier steht ein schönes Zitat zum Thema, lorem ipsum dolor poriaec esci mus Iqui blab ipita.“</p>
-          <p>Rehenda quibea parum cum quodit milla quam ea cus delest id quis aut offic tem re volupie nitiur rero et es esed que sitatus, to ex et quas a dolore dero omnihitam que res ne voluptam, etum sed quodion nitati omnietur, con et que od evelignam fuga. Aruntius. Lorror aliquia vel ipsae digendent la derrovi tatest eum consendita int ommolup tiunt. Boriti omniaeptat. Xerferi tiandandis eum eos esut. Minum quias rum sunti quunt qui.</p>
-        </div> -->
+        <div class="placeholder--comp-book"/>
+        <h3 class="is-h4">Digitalisierung alter Schriften, großes Interesse neuer Schriftgestalter</h3>
+        <p>Hier dazu gehören Adobes Hidden Treasures, lorem ipsum dolor expedi alit int laccabo rporesequat. Acepta pedio eat eum ad eume consequia vendis etur. Atur, sitempo rporitatur Genderferum fugiatiusda qui ilic tem voleni non peris asimet lacid utatemo ssimagnam quae se nimagnatquam quo qui nima et quaecerum cumquati ut faccus, sunt quas dolore molupta turiam et omnis ipic tem et eos et volupid ut repe pe dolorum</p>
+        <p>Ipsam venducipsam ut maxim aut fuga. Ad eaqui officid quos dolore nimus as res dest, id ut alibusam et landam ut aut ut res accum et volesti doluptatus sequam eum sam fugiae pa quidemp ercitae sit, vendanis et ommolorrum ea asit qui omnisi quunt, qui arumque posam voluption exernat. Cae dolum ut voloriam facearum sequam, que mod que vernate sequid.</p>
+        <h3 class="is-h4">Jeder kann heute mit Schrift arbeiten</h3>
+        <p>Heutige Werkzeuge erlauben es eigentlich jedem Schriften zu verwenden und typografisch damit zu spielen. Mit der Adobe Creative Cloud und Typekit gibt es da die perfekten Werkzeuge. Lorem ipsum dolor alitionseque quatur simusapiciam vereium sum ipienessum in renimagnis etur am sintureped qui accat fuga. Ita non pore pro expliquo et aliqui conet asit pore vendigeni ut veriat.</p>
+        <p>Nat fugiaes sume expero occate cusandaecus enda voluptae voluptate labor sam lition rem quaspe volesen isinveles et faceaquat et alibusa vent officit assusda nullaboratur seque odit vollect ustiantotae offici alibus essi cone natur magniae voluptat eaquatur reptatis natur, consequas cusam.</p>
       </div>
     </div>
   </div>
@@ -49,12 +44,11 @@
 <script>
 import Lottie from "@/components/lottie"
 import typewriterAnimation from "@/assets/animations/typewriter.json"
-import StopSign from "@/assets/icons/stop-sign.svg"
+import computerBookAnimation from "@/assets/animations/computer-book.json"
 
 export default {
   components: {
-    Lottie,
-    StopSign
+    Lottie
   },
   data() {
     return {
@@ -63,8 +57,7 @@ export default {
         autoplay: false
       },
       typewriterClasses: "",
-      stopSignClasses: "",
-      isTypewriterPlaceholderAcive: false
+      compBookClasses: ""
     }
   },
   computed: {
@@ -73,22 +66,31 @@ export default {
         animationData: typewriterAnimation,
         ...this.defaultOptions
       }
+    },
+    compBookOptions() {
+      return {
+        animationData: computerBookAnimation,
+        ...this.defaultOptions
+      }
     }
   },
   methods: {
     handleTypewriter: function(anim) {
       this.animTypewriter = anim
     },
+    handleCompBook: function(anim) {
+      this.animCompBook = anim
+    },
     handleTypewriterView(e) {
       if (e.percentTop < 0.8) {
-        this.isTypewriterPlaceholderAcive = true
         this.typewriterClasses = "visible"
         this.animTypewriter.play()
       }
     },
-    handleStopSignView(e) {
+    handleCompBookView(e) {
       if (e.percentTop < 0.8) {
-        this.stopSignClasses = "visible"
+        this.compBookClasses = "visible"
+        this.animCompBook.play()
       }
     }
   }

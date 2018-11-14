8a35b9b3d59b6997949bf05a0276d1295ddd0ef3
Samuel Snopko <samuell.snopko@gmail.com>
2018-11-14 19:01:03 +0100

    datenshutz lnk updated

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="row--outer">
       <div class="column--text">
         <p class="small">Die Süddeutsche Zeitung ist weder für den Inhalt der Anzeige noch die darin enthaltenen Verlinkungen noch für ggf. angegebene Produkte verantwortlich.</p>
-        <a href="http://sz.de">DATENSCHUTZ</a>
+        <a href="https://datenschutz.sueddeutsche.de/sz">DATENSCHUTZ</a>
       </div>
     </div>
   </footer>

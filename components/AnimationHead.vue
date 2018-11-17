0249143bd5b07854121142a062f402c063f4cf80
Samuel Snopko <samuell.snopko@gmail.com>
2018-11-17 14:26:14 +0100

    styled head animation for mobile

--- components/AnimationHead.vue
+++ components/AnimationHead.vue
@@ -1,44 +1,63 @@
 <template>
   <div
     :id="relativeElement"
+    :style="affixWrapperStyleObject"
     class="wrapper--affix-head">
-    <no-ssr>
-      <affix
-        :relative-element-selector="`#${relativeElement}`"
-        :offset="affixOffset"
-        style="width: 100%">
-        <div class="content--affix-head">
-          <div
-            v-view="handleHeadView"
-            :class="headClasses"
-            class="animation--head">
-            <WhiteTriangle
-              :class="triangleClasses"
-              class="white-triangle"/>
-            <div class="lottie--head">
-              <Lottie
-                :options="headOptions"
-                @animCreated="handleHead"/>
-            </div>
+    <div ref="affixContent">
+      <no-ssr>
+        <affix
+          :relative-element-selector="`#${relativeElement}`"
+          :offset="affixOffset"
+          style="width: 100%">
+          <div class="content--affix-head">
             <div
-              v-view="handleTextView"
-              :class="textClasses"
-              class="lottie--brain-text">
-              <Lottie
-                :options="brainTextOptions"
-                @animCreated="handleBrainText"/>
-            </div>
-            <div
-              :class="textClasses"
-              class="lottie--view-text">
-              <Lottie
-                :options="viewTextOptions"
-                @animCreated="handleViewText"/>
+              v-view="handleHeadView"
+              :class="headClasses"
+              class="animation--head">
+              <WhiteTriangle
+                :class="triangleClasses"
+                class="white-triangle"/>
+              <div class="lottie--head">
+                <Lottie
+                  :options="headOptions"
+                  @animCreated="handleHead"/>
+              </div>
+              <div
+                v-view="handleTextView"
+                :class="textClasses"
+                class="lottie--brain-text">
+                <Lottie
+                  :options="brainTextOptions"
+                  @animCreated="handleBrainText"/>
+              </div>
+              <div
+                :class="textClasses"
+                class="lottie--view-text">
+                <Lottie
+                  :options="viewTextOptions"
+                  @animCreated="handleViewText"/>
+              </div>
             </div>
           </div>
-        </div>
-      </affix>
-    </no-ssr>
+          <div class="content--affix-text row--outer">
+            <div class="column--text">
+              <h4>Der erste Eindruck zählt</h4>
+              <p>Was passiert aber, wenn ein Wort im falschen Kleid steckt? Es irritiert und verwirrt uns. Wenn wir beispielsweise das Wort „Liebe“ lesen, fühlen wir uns mit einer eleganten, freundlichen Schrift wohl. So kennen wir das. Schreiben wir „Liebe“ in der Schriftart „Fraktur“, die an NS- Propaganda erinnert, löst dies völlig andere Emotionen in uns aus.</p>
+              <div class="quote-wrapper">
+                <p
+                  v-view="handleQuoteView"
+                  :class="quoteClasses"
+                  class="quote">
+                  „Wir alle konsumieren Typografie. Schriften spielen eine große Rolle in unserem Alltag. Sie helfen uns zu navigieren, Entscheidungen zu treffen und einzukaufen. Sie geben uns Sicherheit und tricksen uns manchmal aus.“
+                  <strong>Sarah Hyndman<br><span>Grafikdesignerin</span></strong>
+                </p>
+                <p>Mit dieser Emotionalität wissen auch Marken umzugehen und überlegen sich ganz genau, in welchem Kleid sie sich präsentieren – denn schließlich soll uns auch über die Schrift vermittelt werden, für was die Marke steht. Eine Anwaltskanzlei wählt wohl eher eine schlichte, schnörkellose Schrift als eine verspielte. Und eine Kosmetikmarke würde sich mit einer technischen Schrift keinen Gefallen tun.</p>
+              </div>
+            </div>
+          </div>
+        </affix>
+      </no-ssr>
+    </div>
   </div>
 </template>
 
@@ -61,6 +80,10 @@ export default {
       type: Number,
       required: true
     },
+    windowWidth: {
+      type: Number,
+      required: true
+    },
     windowScrolled: {
       type: Number,
       required: true
@@ -72,12 +95,9 @@ export default {
         loop: false,
         autoplay: false
       },
+      affixContentHeight: 0,
       relativeElement: "head-animation",
       framesEnteredNumber: 0,
-      affixOffset: {
-        top: 0,
-        bottom: 0
-      },
       triangleClasses: "",
       headClasses: "",
       head: {
@@ -94,10 +114,40 @@ export default {
         isAnimating: false,
         headAllowed: false,
         isAnimationReady: false
-      }
+      },
+      quoteClasses: ""
     }
   },
   computed: {
+    affixWrapperStyleObject() {
+      let height = ""
+      let heightFactor = 2.24
+
+      console.log(this.affixContentHeight)
+      if (this.affixContentHeight - this.windowHeight > 0) {
+        // if is the content of affix bigger than window - add more scrollig distance
+        heightFactor +=
+          (this.affixContentHeight - this.windowHeight) / this.windowHeight
+      }
+
+      if (this.windowWidth < 740) {
+        height = `${this.windowHeight * heightFactor}px`
+      }
+
+      return {
+        height
+      }
+    },
+    affixOffset() {
+      let top = 0
+      // if (this.windowWidth < 740) {
+      //   top = 50
+      // }
+      return {
+        top,
+        bottom: 0
+      }
+    },
     headOptions() {
       return {
         animationData: headAnimation,
@@ -127,6 +177,21 @@ export default {
         this.animateHeadOnScroll(newValue)
         this.animateTextOnScroll(newValue)
       }
+    },
+    windowWidth: {
+      immediate: true,
+      deep: true,
+      handler(newValue, oldValue) {
+        // this.animateHeadOnScroll(newValue)
+        // this.animateTextOnScroll(newValue)
+      }
+    }
+  },
+  mounted() {
+    if (process.browser) {
+      window.onNuxtReady(() => {
+        this.affixContentHeight = this.$refs.affixContent.offsetHeight
+      })
     }
   },
   methods: {
@@ -194,7 +259,7 @@ export default {
           (scrolled - this.text.startHeight) /
           (this.text.endHeight - this.text.startHeight)
         const time = this.text.duration * timePercentage
-        console.log(`${time} + ${this.text.duration}`)
+        // console.log(`${time} + ${this.text.duration}`)
         if (time < this.text.duration) {
           this.animViewText.goToAndStop(time)
           this.animBrainText.goToAndStop(time)
@@ -204,14 +269,10 @@ export default {
         }
       }
     },
-    startTextAnimations() {
-      if (this.framesEnteredNumber === 200) {
-        this.animBrainText.setSpeed(2)
-        this.animBrainText.play()
-        this.animViewText.setSpeed(2)
-        this.animViewText.play()
+    handleQuoteView(e) {
+      if (e.percentTop < 0.8) {
+        this.quoteClasses = "visible"
       }
-      this.framesEnteredNumber += 1
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -13,21 +13,8 @@
     </BaseText>
     <AnimationHead
       :window-height="windowHeight"
+      :window-width="windowWidth"
       :window-scrolled="windowScrolled"/>
-    <BaseText class="under-head-text">
-      <h4>Der erste Eindruck zählt</h4>
-      <p>Was passiert aber, wenn ein Wort im falschen Kleid steckt? Es irritiert und verwirrt uns. Wenn wir beispielsweise das Wort „Liebe“ lesen, fühlen wir uns mit einer eleganten, freundlichen Schrift wohl. So kennen wir das. Schreiben wir „Liebe“ in der Schriftart „Fraktur“, die an NS- Propaganda erinnert, löst dies völlig andere Emotionen in uns aus.</p>
-      <div class="quote-wrapper">
-        <p
-          v-view="handleQuoteView"
-          :class="quoteClasses"
-          class="quote">
-          „Wir alle konsumieren Typografie. Schriften spielen eine große Rolle in unserem Alltag. Sie helfen uns zu navigieren, Entscheidungen zu treffen und einzukaufen. Sie geben uns Sicherheit und tricksen uns manchmal aus.“
-          <strong>Sarah Hyndman<br><span>Grafikdesignerin</span></strong>
-        </p>
-        <p>Mit dieser Emotionalität wissen auch Marken umzugehen und überlegen sich ganz genau, in welchem Kleid sie sich präsentieren – denn schließlich soll uns auch über die Schrift vermittelt werden, für was die Marke steht. Eine Anwaltskanzlei wählt wohl eher eine schlichte, schnörkellose Schrift als eine verspielte. Und eine Kosmetikmarke würde sich mit einer technischen Schrift keinen Gefallen tun.</p>
-      </div>
-    </BaseText>
     <PizzaSection
       :window-height="windowHeight"
       :window-scrolled="windowScrolled"/>
@@ -69,13 +56,16 @@ export default {
     return {
       containerId: "main-body",
       windowHeight: 0,
+      windowWidth: 0,
       windowScrolled: 0,
-      bodyHeight: 0,
-      quoteClasses: ""
+      bodyHeight: 0
     }
   },
   mounted() {
     window.addEventListener("scroll", this.handleScroll)
+    window.addEventListener("resize", this.handleResize)
+    this.windowWidth = window.innerWidth
+    this.windowHeight = window.innerHeight
     console.log("scrolling Injected")
   },
   beforeDestroy() {
@@ -84,18 +74,16 @@ export default {
   },
   methods: {
     handleScroll() {
-      this.windowHeight = window.innerHeight
       this.windowScrolled = window.scrollY
-      this.bodyHeight = document.getElementById(this.containerId).offsetHeight
 
+      // this.bodyHeight = document.getElementById(this.containerId).offsetHeight
       // console.log(`window height : ${this.windowHeight}`)
       // console.log(`scrolled px: ${this.windowScrolled}`)
       // console.log(`body height : ${this.bodyHeight}`)
     },
-    handleQuoteView(e) {
-      if (e.percentTop < 0.8) {
-        this.quoteClasses = "visible"
-      }
+    handleResize() {
+      this.windowWidth = window.innerWidth
+      this.windowHeight = window.innerHeight
     }
   }
 }
@@ -106,89 +94,5 @@ export default {
 
 main {
   overflow: hidden;
-}
-
-.under-head-text {
-  @include clearfix;
-
-  position: relative;
-  z-index: 1;
-  margin-top: rh(-1);
-
-  .quote-wrapper {
-    display: flex;
-    flex-wrap: wrap;
-
-    p {
-      flex-basis: 100%;
-    }
-
-    .quote {
-      @include transition(opacity, transform);
-
-      order: 2;
-      opacity: 0;
-      transform: translate3d(0, 2rem, 0);
-
-      &.visible {
-        opacity: 1;
-        transform: translate3d(0, 0, 0);
-      }
-
-      strong {
-        position: relative;
-        display: block;
-        padding-top: rh(0.5);
-        padding-left: rem(15);
-        font-family: $font-family--header;
-        font-size: rem(15);
-        font-weight: 700;
-
-        &::before {
-          content: "–";
-          position: absolute;
-          top: 0;
-          left: 0;
-          padding-top: rh(0.5);
-        }
-
-        span {
-          font-weight: 100;
-        }
-      }
-    }
-  }
-
-  @include breakpoint(medium) {
-    margin-top: rh(-2);
-  }
-
-  @include breakpoint(large) {
-    .quote-wrapper {
-      display: block;
-
-      .quote {
-        float: left;
-        width: rem(280);
-        padding-right: rem(20);
-        margin-left: rem(-40);
-      }
-    }
-  }
-
-  @include breakpoint(xlarge) {
-    margin-top: rh(-3.5);
-
-    .quote-wrapper {
-      .quote {
-        width: rem(320);
-        margin-left: rem(-160);
-      }
-    }
-  }
-
-  @include breakpoint(xxlarge) {
-    margin-top: rh(-5);
-  }
 }
 </style>
